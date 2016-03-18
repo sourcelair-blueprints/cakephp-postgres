7f1b440887377ca62d97f281205b926500a92651
Antonis Kalipetis <akalipetis@gmail.com>
2016-03-18 16:12:06 +0200

    Add app.php file to version control

--- config/app.php
+++ config/app.php
@@ -216,17 +216,17 @@ return [
     'Datasources' => [
         'default' => [
             'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
+            'driver' => 'Cake\Database\Driver\Postgres',
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => 'postgres',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
+            'username' => 'postgres',
+            'password' => 'mysecretpassword',
             'database' => 'my_app',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
